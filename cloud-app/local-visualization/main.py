a4385571fc2e596dc7a1b523dc70d338ae431504
Thomas Ludescher <thomas@qbee.io>
2022-04-05 11:42:41 +0200

    changing modbus config; connecting to plot_server

--- cloud-app/local-visualization/main.py
+++ cloud-app/local-visualization/main.py
@@ -1,10 +1,10 @@
-
+import requests
 import time 
 import json 
 
 from utils.utility_functions import read_ext_config
 
-from utils.log import setup_logger, update_logger_verbose_level_from_config_file
+from utils.log import setup_logger, update_logger_verbose_level_from_config_file,update_logger_verbose_level
 from collections import deque
 
 from internal_broker_subscriber.internal_broker_subscriber import MosquittoMQTTSubscriber
@@ -21,11 +21,15 @@ def start_mqtt_cloud_app(logger, topic, interval):
  
     # Main loop start
     while True:
+        process_payloads(logger, cloud_mqtt_broker_queue)
         time.sleep(interval)
-        payload = process_payloads(logger, cloud_mqtt_broker_queue)
-        if payload is not None:
-            print("********  Process received payload for publishing into local HTTP Server for Data Visualization *****")
-            print(payload)
+        #if payload is not None:
+        #    print("\n")
+        #    print("********  Process received payload for publishing into local HTTP Server for Data Visualization *****")
+        #    #print(payload)
+        #    send_to_server(payload)
+        #    print("sleep interval is: ", interval)
+        #    print("\n")
     
     
 def process_payloads(logger, cloud_mqtt_broker_queue):
@@ -57,14 +61,27 @@ def process_payloads(logger, cloud_mqtt_broker_queue):
     }
     """
     try:
-        if len(cloud_mqtt_broker_queue) != 0:
+        while len(cloud_mqtt_broker_queue) != 0:
             payload = cloud_mqtt_broker_queue.popleft()
             payload = json.loads(payload)
             # Insert your code for payload processing 
-            return json.dumps(payload)
+            #return payload #json.dumps(payload)
+            send_to_server(payload)
     except Exception as ex:
         logger.error("Caught an Exception when getting queue item. Exception {}:".format(ex))
         return
+
+def send_to_server(payload):
+
+    host = 'localhost'
+    port = 8080
+    url = "http://" + host + ":" + str(port)
+
+    data_arr = payload["data"]
+    for item in data_arr:
+        data = {'tag': item["tagName"],'value': item["tagValue"], 'ts': item["ts"]}
+        print(data)
+        requests.post(url,json=data)
 
     
 def main():
@@ -72,6 +89,7 @@ def main():
     # Read configuretaion file into python object 
     config_int_broker = read_ext_config('resources/config-internal-broker.json')
     update_logger_verbose_level_from_config_file(logger, config_int_broker)
+    update_logger_verbose_level(logger,1)
     
     start_mqtt_cloud_app(logger, topic=config_int_broker["general"]["topic"], interval=config_int_broker["general"]["subscribe_interval"])
     
